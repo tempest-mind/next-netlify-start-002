562ece9d72a4c23ff47e1b7634131a0955dfb774
tempest-mind <71837023+tempest-mind@users.noreply.github.com>
2021-04-30 21:39:28 -0700

    Update index.js
    version update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to the Main branch! v0.2" />
+        <Header title="Welcome to the Main branch! v0.3" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
